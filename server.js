2bd45660c06fa1d0d89c5624c6bdd6a83b4eacce
Maserall Marcelin <maserallm@knights.ucf.edu>
2020-07-13 20:55:54 -0400

    added clusters to server

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+
 const express = require("express");
 const expressHbrs = require("express-handlebars");
 const mongoose = require("mongoose");
-const logger = require("morgan");
+const morgan = require("morgan");
 
 const PORT = process.env.PORT || 3006;
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -26,13 +29,34 @@ app.set("view engine", "handlebars");
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/nba-news-scraper";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 require("./routes/sportroutes")(app);
 require("./routes/htmlroutes")(app);
 
-app.listen(PORT, function() {
-  console.log(
-    "App running on port 3000! Click Here to access http://localhost:3000"
-  );
-});
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // Workers can share any TCP connection
+  // In this case it is an HTTP server
+
+  app.listen(PORT, function() {
+    console.log(
+      "App running on port 3006! Click Here to access http://localhost:3006"
+    );
+  });
+
+  console.log(`Worker ${process.pid} started`);
+}
